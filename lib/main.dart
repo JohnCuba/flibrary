02b285ba6bede15128a94f93ba0fc2996fdd5d79
Evgeny Kubecky <johncuba@yandex.ru>
2021-12-23 21:50:36 +0300

    feat: init provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './screens/main.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Main()));
+  runApp(const ProviderScope(child: MaterialApp(home: Main())));
 }
--- lib/screens/device.dart
+++ lib/screens/device.dart
@@ -1,50 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
 
 import '../components/bookPreview.dart';
+import 'package:flibrary/providers/device.dart';
 
-class DeviceScreen extends StatefulWidget {
+class DeviceScreen extends ConsumerStatefulWidget {
   const DeviceScreen({Key? key}) : super(key: key);
 
   @override
-  State<DeviceScreen> createState() => _DeviceScreenState();
+  _DeviceScreenState createState() => _DeviceScreenState();
 }
 
-class _DeviceScreenState extends State<DeviceScreen> {
-  late String _directoryPath;
+class _DeviceScreenState extends ConsumerState<DeviceScreen> {
   late List<FileSystemEntity> _books;
 
   @override
   void initState() {
-    _directoryPath = '';
     _books = [];
     super.initState();
+    ref.read(deviceProvider);
   }
 
   _pickDirectory() async {
+    final deviceState = ref.read(deviceProvider);
     String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    deviceState.updateSourcePath(selectedDirectory ?? '');
     Directory directory = Directory(selectedDirectory ?? '');
 
     setState(() {
-      _directoryPath = selectedDirectory ?? '';
       _books = directory.listSync(recursive: false);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final deviceState = ref.watch(deviceProvider);
+
     return Column(
       children: [
         AppBar(
           leading: IconButton(
-            icon: Icon(_directoryPath.isNotEmpty
+            icon: Icon(deviceState.sourcePath.isNotEmpty
                 ? Icons.devices_outlined
                 : Icons.device_unknown_outlined),
             onPressed: _pickDirectory,
           ),
-          title: Text(_directoryPath.isNotEmpty
-              ? _directoryPath
+          title: Text(deviceState.sourcePath.isNotEmpty
+              ? deviceState.sourcePath
               : 'Выбери папку с книгами на устройстве'),
         ),
         ..._books.map((e) => BookPreview(title: e.path)).toList()
