c7e12e2c6d70d7cc4661659a3e3d5b93c7abd563
JohnCuba <johncuba@yandex.ru>
2022-01-04 18:53:26 +0300

    fix: add placeholder on device screen

--- lib/screens/device.dart
+++ lib/screens/device.dart
@@ -14,9 +14,27 @@ class DeviceScreen extends ConsumerWidget {
 
     return SizedBox(
       height: MediaQuery.of(context).size.height,
-      child: Gallery(
-        children: deviceState.files.map((e) => BookPreview(file: e)).toList(),
-      ),
+      child: deviceState.dirSetted 
+        ? Gallery(
+          children: deviceState.files.map((e) => BookPreview(file: e)).toList(),
+        ) 
+        : Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                splashRadius: 16,
+                icon: Icon(deviceState.dirSetted
+                    ? Icons.devices_outlined
+                    : Icons.device_unknown_outlined),
+                onPressed: deviceState.chooseSourcePath,
+              ),
+              Text(deviceState.dirSetted
+                  ? deviceState.dir!.path
+                  : 'Выбери папку с книгами на устройстве'),
+            ],
+          ),
+        ),
     );
   }
 }
